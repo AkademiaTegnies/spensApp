a32c33f57ae499c945184e3673f80e6b7b2d7cb2
AkademiaTegnies <tegnies@akademia.ac.za>
2016-02-23 09:06:45 +0200

    Fixed date day time filter

--- www/core/filters/date_day_or_time.js
+++ www/core/filters/date_day_or_time.js
@@ -31,18 +31,11 @@ angular.module('mm.core')
  *
  * This filter expects a timestamp NOT including milliseconds.
  */
-.filter('mmDateDayOrTime', function($translate) {
+.filter('mmDateDayOrTime', function($filter) {
+    var angularDateFilter = $filter('date');
     return function(timestamp) {
-        return moment(timestamp * 1000).calendar(null, {
-            /*
-            sameDay: $translate.instant('mm.core.dftimedate'),
-            lastDay: $translate.instant('mm.core.dflastweekdate'),
-            lastWeek: $translate.instant('mm.core.dflastweekdate')
-            */
-            sameDay: 'mm.core.dftimedate',
-            lastDay: 'mm.core.dflastweekdate',
-            lastWeek: 'mm.core.dflastweekdate'
-        });
+        return angularDateFilter(timestamp, 'dd MMMM teen HH:mm:ss');
     };
 
 });
+
